14aef94d4e23179820e234d88ecf6ef02cadc642
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-12-16 16:49:47 -0300

    Tela de login + Container de login ok

--- lib/src/modules/login/login_page.dart
+++ lib/src/modules/login/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../core/ui/helper/size_extension.dart';
 import '../../core/ui/styles/colors_app.dart';
+import '../../core/ui/styles/text_styles.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -38,27 +39,56 @@ class LoginPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            constraints: BoxConstraints(
-              maxWidth: context.percentWidth(screenWidth < 1200 ? .4 : .3),
-              // maxHeight: context.percentHeight(.5),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text('Logo'),
-                  Text('login'),
-                  Text('TextformFiel'),
-                  Text('TextformFiel'),
-                  Text('Esqueceu senha'),
-                  Text('btn'),
-                ],
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: context.percentWidth(screenWidth < 1200 ? .7 : .4),
+                // maxHeight: context.percentHeight(.5),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // Text('Logo'),
+                      FractionallySizedBox(
+                        widthFactor: 0.4,
+                        child: Image.asset('assets/images/logo.png'),
+                      ),
+                      const SizedBox(height: 20),
+                      FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Text('login', style: context.textStyles.textTitle),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Email'),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Senha'),
+                      ),
+                      const SizedBox(height: 20),
+                      Text('Esqueceu senha?'),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            //!AQUI
+                          },
+                          child: Text('Entrar'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           )
