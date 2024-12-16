402efd197e9546b972495c4ff89295cbd5ca144c
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-12-16 16:13:01 -0300

    acertando imagem de abertura logo e tamanho max

--- lib/src/modules/login/login_page.dart
+++ lib/src/modules/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../core/ui/helper/size_extension.dart';
 import '../../core/ui/styles/colors_app.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,20 +8,33 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenShortsSide = context.screenShortestSize;
+
     return Scaffold(
       //  appBar: AppBar(title: const Text('Login_Page')),
       backgroundColor: context.colors.blacky,
       body: Stack(
         alignment: Alignment.topCenter,
         children: [
-          Container(
-            height: context.,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/lanche.png'),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: screenShortsSide * .5,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/lanche.png'),
+                ),
               ),
             ),
           ),
+          Padding(
+            padding:  EdgeInsets.only(top: context.percentHeight(0.1)),
+            child: SizedBox(
+              width: screenShortsSide *0.5,
+              child: Image.asset('assets/images/logo.png', fit: BoxFit.cover,),
+            ),
+          ),
+          Container(constraints: BoxConstraints(),)
         ],
       ),
     );
