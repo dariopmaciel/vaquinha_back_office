27a9f84cf5cbc725f8c609d737262175d052cedf
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-12-16 16:23:06 -0300

    campo de login

--- lib/src/core/ui/helper/size_extension.dart
+++ lib/src/core/ui/helper/size_extension.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 extension SizeExtension on BuildContext {
-  double get screenWidget => MediaQuery.of(this).size.width;
+  double get screenWidth => MediaQuery.of(this).size.width;
   double get screenHeight => MediaQuery.of(this).size.height;
   double get screenShortestSize => MediaQuery.of(this).size.shortestSide;
   double get screenLongestSize => MediaQuery.of(this).size.longestSide;
 
-  double percentWidget(double percent) => screenWidget * percent;
+  double percentWidth(double percent) => screenWidth * percent;
   double percentHeight(double percent) => screenHeight * percent;
 }
--- lib/src/modules/login/login_page.dart
+++ lib/src/modules/login/login_page.dart
@@ -9,6 +9,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenShortsSide = context.screenShortestSize;
+    final screenWidth = context.screenWidth;
 
     return Scaffold(
       //  appBar: AppBar(title: const Text('Login_Page')),
@@ -28,13 +29,38 @@ class LoginPage extends StatelessWidget {
             ),
           ),
           Padding(
-            padding:  EdgeInsets.only(top: context.percentHeight(0.1)),
+            padding: EdgeInsets.only(top: context.percentHeight(0.1)),
             child: SizedBox(
-              width: screenShortsSide *0.5,
-              child: Image.asset('assets/images/logo.png', fit: BoxFit.cover,),
+              width: screenShortsSide * 0.5,
+              child: Image.asset(
+                'assets/images/logo.png',
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-          Container(constraints: BoxConstraints(),)
+          Container(
+            constraints: BoxConstraints(
+              maxWidth: context.percentWidth(screenWidth < 1200 ? .4 : .3),
+              // maxHeight: context.percentHeight(.5),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                children: [
+                  Text('login'),
+                  Text('login'),
+                  Text('TextformFiel'),
+                  Text('TextformFiel'),
+                  Text('Esqueceu senha'),
+                  Text('btn'),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
