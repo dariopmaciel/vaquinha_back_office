ab6594cf3c37c1fc58c6081c454a7a04bfec69b8
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-12-17 08:30:58 -0300

    Camada DIO montada...

--- lib/src/core/rest_cliente/custom_dio.dart
+++ lib/src/core/rest_cliente/custom_dio.dart
@@ -2,6 +2,22 @@ import 'package:dio/browser.dart';
 import 'package:dio/dio.dart';
 
 class CustomDio extends DioForBrowser {
-    //
-    CustomDio():super(BaseOptions(baseUrl: ''));
+  //
+  CustomDio()
+      : super(
+          BaseOptions(
+            baseUrl: 'backend_base_url',
+            connectTimeout: const Duration(seconds: 5),
+            receiveTimeout: const Duration(seconds: 60),
+          ),
+        ) {
+    interceptors.add(
+      LogInterceptor(
+        request: true,
+        responseBody: true,
+        requestHeader: true,
+        responseHeader: true,
+      ),
+    );
+  }
 }
