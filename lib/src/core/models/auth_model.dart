5668f18b607c1596dc35d9785d9d881751d69d9f
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-12-17 09:20:06 -0300

    auth model parao dio

--- lib/src/core/models/auth_model.dart
+++ lib/src/core/models/auth_model.dart
@@ -1,4 +1,28 @@
+import 'dart:convert';
 
 class AuthModel {
-  
+  // "email": "rodrigorahman@academiadoflutter.com.br",
+  //   "password": "123123",
+  //   "admin": true
+  //!RECEBE 'access_token'
+
+  final String accessToken;
+
+  AuthModel({required this.accessToken});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'accessToken': accessToken,
+    };
+  }
+
+  factory AuthModel.fromMap(Map<String, dynamic> map) {
+    return AuthModel(
+      accessToken: map['accessToken'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AuthModel.fromJson(String source) => AuthModel.fromMap(json.decode(source));
 }
